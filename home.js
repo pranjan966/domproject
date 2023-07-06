b25e8bac07c1f5a079a21097b5f0e6c5b436647b
pranjan966 <108427201+pranjan966@users.noreply.github.com>
2023-07-06 22:04:00 +0530

    Added delete btn in li,methord defied for delete btn

--- home.js
+++ home.js
@@ -4,30 +4,52 @@ const listItems = document.getElementById("items")
 
 
 
+
 formInfo.addEventListener("submit", submit);
+listItems.addEventListener("click", deleteItem)
 // // listItems.addEventListener("click", removeItem);
 
 function submit(e) {
     e.preventDefault();
+    formInfo.style.background = "#f4f4f4"
     const email = document.getElementById("inputEmail").value
     const city = document.getElementById("inputCity").value
-    console.log(email, city)
+
+    const myObj = {
+        email: `${email}`,
+        city: `${city}`
+    }
+    // console.log(myObj)
     // localStorage.setItem("user_email", email)
-    const email_serialised = JSON.stringify(email)
-    localStorage.setItem("email", email_serialised)
-    const email_deserialized = JSON.parse(localStorage.getItem("email"))
-    console.log(email_deserialized)
+    const myObj_serialised = JSON.stringify(myObj)
+    localStorage.setItem("email", myObj_serialised)
+    const myObj_deserilized = JSON.parse(localStorage.getItem(myObj))
+    // console.log(myObj_deserilized)
 
 
     // newItem = document.getElementById("items").value;
     const li = document.createElement("li")
     li.className = "list-group-item"
     li.appendChild(document.createTextNode(`* ${email} ${city}`))
+    const deleteBtn = document.createElement("delete");
+    deleteBtn.className = "btn btn-danger btn-sm float-right delete"
+    deleteBtn.appendChild(document.createTextNode("Delete"))
+    li.appendChild(deleteBtn)
+
 
 
     listItems.appendChild(li)
 
 
 
+
+}
+function deleteItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure')) {
+            const li = e.target.parentElement;
+            listItems.removeChild(li);
+        }
+    }
 
 }
